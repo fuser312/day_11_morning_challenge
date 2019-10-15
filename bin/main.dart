d064469243ab8915bdd242ffc27f796aab5426a1
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-15 15:44:04 +0530

    Day 11 Morning Challenge complete.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 // Challenge 1
 // Create a function that returns the total number of steps it takes to transform each element to the maximal element in the list.
 // Each step consists of incrementing a digit by one.
@@ -7,6 +9,14 @@
 // incrementToTop([4, 3, 4]) ➞ 1
 // incrementToTop([3, 3, 3]) ➞ 0
 // incrementToTop([3, 10, 3]) ➞ 14
+
+int incrementToTop(List list){
+  int count = 0;
+  list.sort();
+  int maximum = list.last;
+  list.forEach((element)=> count = count +(maximum - element));
+  return count;
+}
 
 // Challenge 2
 // In BlackJack, cards are counted with -1, 0, 1 values:
@@ -21,7 +31,34 @@
 //
 //  count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]) ➞ 5
 
+int count(List list) {
+  Map blackjack = {
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 1,
+    10: -1,
+    "J": -1,
+    "Q": -1,
+    "K": -1,
+    "A": -1,
+    7:0,
+    8:0,
+    9:0,
+  };
+  int count = 0;
+  list.forEach((element) => count = count + blackjack[element]);
+  return count;
+}
 // Challenge 3
 // Write a test case each for previous two functions you wrote
 
-main() {}
+main() {
+
+  print(incrementToTop([3, 4, 5]));
+  print(incrementToTop([4, 3, 4]));
+  print(incrementToTop([3, 3, 3]));
+
+  print(count(["A", "A", "K", "Q", "Q", "J"]));
+}
--- bin/test.dart
+++ bin/test.dart
@@ -1,3 +1,14 @@
 import 'package:test/test.dart';
+import 'main.dart';
 
-void main() {}
+
+void main() {
+  test("test case 1", () {
+    expect(incrementToTop([3, 10, 3]), 14);
+    expect(incrementToTop([3, 3, 3]), 0);
+  });
+  test("test case 2", () {
+    expect(count(["A", "A", "K", "Q", "Q", "J"]), -6);
+    expect(count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]), 5);
+  });
+}
